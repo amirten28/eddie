533a004a4d7edcfc44d4ebbec4fc7ac6db7a6c09
amirten28 <amirten28@gmail.com>
2019-06-10 14:20:34 +0300

    Add files via upload

---
+++ appgo.js
@@ -0,0 +1,47 @@
+var red = document.getElementById('a');
+var blue = document.getElementById('b');
+var green = document.getElementById('c');
+var yellow = document.getElementById('d');
+var pink = document.getElementById('e');
+var orange = document.getElementById('f');
+var black = document.getElementById('g');
+var white = document.getElementById('h');
+var purple = document.getElementById('i');
+
+
+function play1(){
+	var audio = document.getElementById("aa");
+       audio.play(); 
+}
+function play2(){
+	var audio = document.getElementById("bb");
+       audio.play(); 
+}
+function play3(){
+	var audio = document.getElementById("cc");
+       audio.play(); 
+}
+function play4(){
+	var audio = document.getElementById("dd");
+       audio.play(); 
+}
+function play5(){
+	var audio = document.getElementById("ee");
+       audio.play(); 
+}
+function play6(){
+	var audio = document.getElementById("ff");
+       audio.play(); 
+}
+function play7(){
+	var audio = document.getElementById("gg");
+       audio.play(); 
+}
+function play8(){
+	var audio = document.getElementById("hh");
+       audio.play(); 
+}
+function play9(){
+	var audio = document.getElementById("ii");
+       audio.play(); 
+}
